74b7be4ed0b62e09e83a59df01b977e6f6efd7d5
mheydt <mike@heydt.org>
2015-02-02 09:42:09 -0700

    Updates slides and outlines

---
+++ Reference/Particular/DataBus/DeleteAllQueues/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeleteAllQueues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DeleteAppQueues();
+            
+        }
+
+        public static void DeleteAppQueues()
+        {
+            List<string> trash = new List<string>();
+
+            var machineQueues = MessageQueue.GetPrivateQueuesByMachine(".");
+            foreach (var q in machineQueues)
+            {
+//#if (IsAppQueue(q.QueueName))
+                {
+                    trash.Add(".\\" + q.QueueName);
+                } 
+                q.Dispose();
+            }
+
+            foreach (var queueName in trash)
+            {
+                try
+                {
+                    using (MessageQueue delQueue = new MessageQueue(queueName))
+                    {
+                        delQueue.SetPermissions("Everyone", MessageQueueAccessRights.FullControl, AccessControlEntryType.Allow);
+                    }
+                    MessageQueue.Delete(queueName);
+                }
+                catch (MessageQueueException ex)
+                {
+                    // ex.Message is "Access to Message Queuing system is denied."
+                }
+            }
+        }
+    }
+
+}
